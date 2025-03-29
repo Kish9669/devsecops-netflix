9964449c0e8122f264e927b3c9f988db676cfe42
Kish9669 <163889606+Kish9669@users.noreply.github.com>
2025-03-29 17:16:01 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-kish9669" # Replace with your actual S3 bucket name
+    bucket = "devsecops-netflix-rkish" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
